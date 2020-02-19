6dfc759a608928a3467be5ae86256403a7b0d964
Bradley Drummonds <47236494+bdrummo6@users.noreply.github.com>
2020-02-19 05:34:39 -0500

    MVP and Some Stretch Met

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,3 +33,42 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+
+const createMenu = (arr) => {
+  // Creates a header element contain the menu and it's elements
+  const menuContainer = document.querySelector('.header');
+
+  // Creates a div which will contain the list and list items that will make up the menu
+  const menu = document.createElement('div');
+  menu.classList.add('menu');
+
+  // Creates an unordered list for holding the list items which are menu items
+  const menuList = document.createElement('ul');
+
+  // Iterates through the array of items and stores them in list items and appends those to the menuList
+  arr.forEach((item) => {
+    const menuItem = document.createElement('li');
+    menuItem.textContent = item;
+    menuList.appendChild(menuItem);
+  });
+
+  // Creates a menu button with the class menu-button and then an event listener for when the button is clicked on
+  const menuBtn = document.querySelector('.menu-button');
+  menuBtn.addEventListener('click', () => {
+    menu.style.color = '#81C784'; // When the menu opens the default font color changes
+    menu.style.backgroundColor = '#FFFEF7'; // When the menu opens the default background color changes
+    menu.classList.toggle('menu--open');
+
+    articles.addEventListener('click', () => { // When a click occurs on the main part of the page then the menu closes
+      menu.classList.remove('menu--open');
+    })
+  });
+
+  menuContainer.appendChild(menu); // Appends the menu div to the menuContainer header
+  menu.appendChild(menuList); // Appends the menuList ul to the menu div
+
+  return menuContainer;
+
+};
+
+createMenu(menuItems);
